cffa0273afff9f6617201945ad1e83ddb3637140
Eric Valverde <ericvalverde@Eric.local>
2015-04-22 16:57:30 +0100

    karma conf change

--- client/test/karma.conf.js
+++ client/test/karma.conf.js
@@ -35,8 +35,8 @@ module.exports = function(config) {
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
-      'test/mock/**/*.js',
-      'test/spec/**/*.js'
+      //'test/mock/**/*.js',
+      'test/spec/unit/**/*.js'
     ],
 
     // list of files / patterns to exclude
