53fcf05a96e61ba2143fc2fdc0832934ba1d5010
Eric Valverde <erictvalverde@Erics-MacBook-Pro.local>
2015-05-31 17:27:11 +0100

    a Few changes

--- client/test/spec/unit/controllers/movie-add.js
+++ client/test/spec/unit/controllers/movie-add.js
@@ -1,19 +1,45 @@
 'use strict';
 
-describe('Controller: MovieAddCtrl', function () {
+describe('Controller: MoviesCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('clientApp'));
+ // load the controller's module
+  beforeEach(module('meanMovies'));
 
   var MovieAddCtrl,
-    scope;
+    scope,
+    httpBackend,
+    Movie,
+    location,
+    response = {"__v":0,"title":"New Movie","url":"NewURL.com","_id":"553956e9b63464e80166a52f","comments":[],"ratings":[]};
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MovieAddCtrl = $controller('MovieAddCtrl', {
+  beforeEach(inject(function ($controller, $rootScope, _$httpBackend_, _$location_, _Movie_) {
+    httpBackend   = _$httpBackend_;
+    Movie         = _Movie_;
+    location      = _$location_;
+    scope         = $rootScope.$new();
+    MovieAddCtrl  = $controller('MovieAddCtrl', {
       $scope: scope
     });
   }));
+
+
+  it('should have a addMovie controller', function() {
+    expect(MovieAddCtrl).toBeDefined();
+  });
+
+  it('should contain an empty movie obj in scope at start', function() {
+    expect(scope.movie).toEqual({});
+  });
+
+  it('should add a new movie', function() {
+
+    spyOn(location, 'path');
+
+    httpBackend.whenPOST('http://localhost:3000/movie').respond('success');
+   
+    expect(location.path).toHaveBeenCalledWith('/movies');
+  });
+
 
 });
